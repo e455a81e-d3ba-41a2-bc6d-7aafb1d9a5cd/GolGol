2797bfd5e755ab2e7c3204dfe33fc800e1608dff
Leonhardt Schwarz <leonhardt@Uranus.(none)>
2013-03-18 01:06:01 +0100

    Let's get started already...

---
+++ algo/algo.hpp
@@ -0,0 +1,15 @@
+#ifndef ALGO_HPP
+#define ALGO_HPP
+
+class algo {
+public:
+	algo (std::vector<gol_t> init_gen);
+	~algo ();
+
+	gol_t next_gen();
+
+
+private:
+	/* data */
+};
+#endif
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <cstdio>
+#include <cstring>
+
+int main(int argc, const char *argv[])
+{
+	cout << "Hello, GOL";
+	return 0;
+}
