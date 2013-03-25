e2d0564abfa24f030b2639285464b9c09ad33321
Leonhardt Schwarz <leonhardt@Uranus.(none)>
2013-03-25 16:12:46 +0100

    algo.hpp angepasst

---
+++ algo/algo.cpp
@@ -0,0 +1,17 @@
+#include "../include/algo.hpp"
+#include <vector>
+
+algo::algo(std::vector<gboolean> i_gen, int size)
+{
+	p_gen = i_gen;
+}
+
+algo::~algo()
+{
+
+}
+
+int algo::next_gen(std::vector<gboolean>& n_gen)
+{
+	
+}
